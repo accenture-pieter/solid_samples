b1e78ab520a28e0b32ff94dbd86b6f88c5f80630
Unknown <snyman.pieter@gmail.com>
2017-12-01 18:01:46 +0200

    [LSP] override width and height

--- SolidSamples_Part3_LSP/app/src/main/java/com/example/solidsamples/bad/kotlin/Types.kt
+++ SolidSamples_Part3_LSP/app/src/main/java/com/example/solidsamples/bad/kotlin/Types.kt
@@ -5,14 +5,26 @@ package com.example.solidsamples.bad.kotlin
  * Created by pieter.snyman on 11/13/17.
  */
 
-class Square(size : Double) : Rectangle(size, size) {
+class Square(size: Double) : Rectangle(size, size) {
     override val area: Double
         get() {
             return width * height
         }
+
+    override var height = size
+        set(value) {
+            field = value
+            width = value
+        }
+
+    override var width = size
+        set(value) {
+            field = value
+            height = value
+        }
 }
 
-open class Rectangle(var width: Double, var height: Double) {
+open class Rectangle(open var width: Double, open var height: Double) {
     open val area: Double
         get() {
             return width * height
